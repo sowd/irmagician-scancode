ae14d1e767be9e824f6e88797e50c73feae4c699
Shigeru Owada <sowd@acm.org>
2017-08-03 14:26:35 +0000

    irMagician.js moved

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-let irMagician = require("irmagician")
-irMagician.info()
+var l = console.log ;
 
+
+let irMagician = require("./irMagician.js")
+//irMagician.info()
+
+irMagician.capture()
+
+l('Hey')
+
+
+/*.then(r=>{
+	l( JSON.stringify(r) )
+}).catch(e=>{
+	l( 'e:'+JSON.stringify(e) )
+})*/
